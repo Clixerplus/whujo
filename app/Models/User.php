6b9a3ac525a3d37ae6cee09471e9055b719c9385
Johan Tovar <jtovarto@gmail.com>
2020-11-09 15:35:34 -0400

    feat: Activa implementacion de Verificacion de email

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,14 +10,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
-    use MustVerifyEmail;
 
     /**
      * The attributes that are mass assignable.
