7a5a26921f4cf94d118a1665bdb5fe2624573b94
Johan Tovar <jtovarto@gmail.com>
2020-11-26 14:31:37 -0400

    feat: Agrega paleta de colores personalizada Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
     // purge: [
@@ -11,6 +12,21 @@ module.exports = {
 
     theme: {
         extend: {
+            colors: {
+                primary: {
+                    light: "#ff2e31",
+                    DEFAULT: "#E32a2e",
+                    dark: "#bd2225",
+                },
+                secondary: {
+                    light: "#1E293B",
+                    DEFAULT: "#0F172A",
+                },
+                success: "#00B74A",
+                info: "#1266F1",
+                danger: "#D50000 ",
+                warning: "#FFA900",
+            },
             boxShadow: {
                 w1:
                     "0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)",
@@ -19,29 +35,18 @@ module.exports = {
                 w3:
                     "0 12px 15px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19)",
                 card:
-                    "0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                    "0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
             },
             fontFamily: {
                 sans: ["Quicksand", ...defaultTheme.fontFamily.sans],
-                whujo: ["arial"]
+                whujo: ["arial"],
             },
-            colors: {
-                primary: {
-                    lighter: "#ff2e31",
-                    DEFAULT: "#E32a2e",
-                    dark: "#bd2225"
-                },
-                secondary: {
-                    lighter: "#CBD5E0",
-                    DEFAULT: "#1A202C"
-                }
-            }
-        }
+        },
     },
 
     variants: {
-        opacity: ["responsive", "hover", "focus", "disabled"]
+        opacity: ["responsive", "hover", "focus", "disabled"],
     },
 
-    plugins: [require("@tailwindcss/ui")]
+    // plugins: [require("@tailwindcss/ui")]
 };
