00e96d573ed1ea5c7b816cd0c017ef96dbfe6b4e
Johan Tovar <jtovarto@gmail.com>
2020-11-28 12:31:33 -0400

    feat(UI): Crea componente Input standard

---
+++ resources/views/components/ui/input.blade.php
@@ -0,0 +1,19 @@
+@props(['name'])
+<div class="relative h-auto">
+
+    <input
+        {{
+    $attributes->merge([
+            'class' => 'w-full rounded-md p-4 pr-16 placeholder-gray-500 border focus:ring focus:ring-gray-200 ' ,
+            'name'  => $name ?? ''
+    ])
+    }}
+    >
+
+    <span class="ml-2 text-danger text-xs ">
+        @error($name)
+        {{ $message }}
+        <x-icon-alert-circle-outline class="absolute inset-0 w-10 h-10 mt-2 mr-2 ml-auto opacity-75" />
+        @enderror
+    </span>
+</div>
