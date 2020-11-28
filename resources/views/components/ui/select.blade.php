509b82ba69d354b783cc6e73da493f23497e3627
Johan Tovar <jtovarto@gmail.com>
2020-11-28 15:58:02 -0400

    feat(UI): Agrega componente Select standard

---
+++ resources/views/components/ui/select.blade.php
@@ -0,0 +1,10 @@
+@props(['name'])
+<select {{
+    $attributes->merge([
+            'class' => 'w-full rounded-md p-4 pr-16 placeholder-gray-500 border focus:ring focus:ring-gray-200 ' ,
+            'name'  => $name ?? ''
+    ])
+    }}>
+    {{ $slot}}
+</select>
+<span class="ml-2 text-danger text-xs ">@error('minimumAge'){{ $message }}@enderror</span>
