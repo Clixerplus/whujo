dda74c538cf1cb73880241b1afecc364f3073596
Johan Tovar <jtovarto@gmail.com>
2020-11-29 18:06:31 -0400

    feat: Implementa archivo de configuracion
    Para estandarizar las opciones, límites y otras configuraciones.

---
+++ config/product.php
@@ -0,0 +1,17 @@
+<?php
+return [
+
+    'skill_levels' => [
+        'Principiante',
+        'Intermedio',
+        'Avanzado',
+        'Experto',
+    ],
+
+    'activity_levels' => [
+        'Leve',
+        'Moderada',
+        'Exigente',
+        'Extrema',
+    ],
+];
