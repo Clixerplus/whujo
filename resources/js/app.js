c91212e2f8a97a1e64444b3851e1b73a01b1612a
Johan Tovar <jtovarto@gmail.com>
2020-12-16 00:41:37 -0400

    feat: Configura clases y sricpts para packeg Embla

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import EmblaCarousel from "embla-carousel";
+const emblaNode = document.getElementById("embla");
+
+const embla = EmblaCarousel(emblaNode);
+
+const closeAlert = () => {
+    const alert = document.getElementById("alert");
+    alert.classList.add("scale-x-0");
+};
+
