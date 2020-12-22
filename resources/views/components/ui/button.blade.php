4531464b23b7e1237b5fad9c1a1d7f5dbeaa35f8
Johan Tovar <jtovarto@gmail.com>
2020-12-21 20:56:26 -0400

    fix(UI): Solventa problema de clases en componentes Button y Select

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,5 +1,5 @@
 <button {{ $attributes->merge([
-    'class' => 'inline-flex items-center justify-center leading-7 border border-transparent rounded-md
+    'class' => 'inline-flex items-center justify-center leading-7 rounded-md
                 text-white active:bg-gray-900 focus:outline-none focus:border-gray-900
                 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150'
             ]) }}>
--- resources/views/components/ui/select.blade.php
+++ resources/views/components/ui/select.blade.php
@@ -1,28 +1,26 @@
 @php
-    if ($errors->has($key))
-        $class = 'ring ring-danger focus:ring-danger ring-opacity-75';
-    else
-        $class = '';
+if ($errors->has($key))
+$class = 'ring ring-danger focus:ring-danger ring-opacity-75';
+else
+$class = '';
 @endphp
 <div class="relative my-2">
     <div class="border-l pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-600">
-        <x-icon-caret-down-circle-outline class="fill-current w-6 h-6"             />
+        <x-icon-caret-down-circle-outline class="fill-current w-6 h-6" />
     </div>
 
     <select {{ $attributes->merge([
-                    'class' => 'w-full rounded-md p-4 pr-16 z-20  appearance-none border focus:ring focus:ring-gray-200 outline-none ' . $class,
+                    'class' => 'w-full rounded-md px-4 pr-16 z-20  appearance-none border focus:ring focus:ring-gray-200 outline-none ' . $class,
                     'name'  => $key
-                ])}}>
-
-        <option class="text-gray-500">{{__('Choose an option')}}</option>
+                ])}} >
 
         {{ $slot }}
 
     </select>
 
     @if ($key)
-        @error($key)
-            <x-icon-alert-circle-outline class="absolute inset-0 my-auto bg-white text-danger w-10 h-10 mr-12 ml-auto" />
-        @enderror
+    @error($key)
+    <x-icon-alert-circle-outline class="absolute inset-0 my-auto bg-white text-danger w-10 h-10 mr-12 ml-auto" />
+    @enderror
     @endif
 </div>
