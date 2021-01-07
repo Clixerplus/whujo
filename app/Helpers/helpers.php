679a424ba92bd7f6d757af96827272357e7414d7
Johan Tovar <jtovarto@gmail.com>
2021-01-07 09:57:55 -0400

    feat: Agrea constante Status, Skill_Lvl, Activity_Lvl
    Para estandarizacion, desacomplamiento y mantenimiento de los modelos y campos relacionados

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,25 @@
 <?php
 
+const UNASSIGNED = 'unassigned';
+
+/** Status Constant */
+const STATUS_INCOMPLETE  = 'incomplete';
+const STATUS_UNPUBLISHED = 'unpublished';
+const STATUS_PUBLISHED   = 'published';
+
+/** Skill Levels Constant */
+const SKILL_LVL_BEGINNER = 'Principiante';
+const SKILL_LVL_MEDIUM   = 'Intermedio';
+const SKILL_LVL_ADVANCED = 'Avanzado';
+const SKILL_LVL_EXPERT   = 'Experto';
+
+/** Skill Levels Constant */
+const ACTIVITY_LVL_LIGHT     = 'Leve';
+const ACTIVITY_LVL_MODERATE  = 'Moderada';
+const ACTIVITY_LVL_DEMANDING = 'Exigente';
+const ACTIVITY_LVL_EXTREME   = 'Extrema';
+
+
 function isActiveRoute($path)
 {
     return Route::is($path) ? 'active' : '';
