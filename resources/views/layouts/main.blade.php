4d839b8e612c95b34fd22fb5052b5de698e4718a
Johan Tovar <jtovarto@gmail.com>
2021-01-13 09:10:01 -0400

    feat(UI): Crea componente Main-Layout para capa principal de plantillas

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        @stack('styles')
+
+    </head>
+
+    <body class="font-sans text-gray-700">
+
+        {{ $slot }}
+
+        @livewireScripts
+    </body>
+
+</html>
