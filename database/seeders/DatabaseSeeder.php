9828394cb8e2748e02bfcd8ee3e5763402acce5a
Johan Tovar <jtovarto@gmail.com>
2021-01-13 23:45:15 -0400

    chore: Agrega y mejoran factorias al seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
 use App\Models\User;
+use App\Models\State;
 use App\Models\Service;
 use App\Models\Category;
+use App\Models\Locality;
 use App\Models\Experience;
+use App\Models\Microservice;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CitySeeder;
+use Database\Seeders\StateSeeder;
+use Database\Seeders\LocalitySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,18 +30,23 @@ class DatabaseSeeder extends Seeder
                 'email' => 'usuario@email.test'
             ]);
 
-        $this->call([
+      /*  $this->call([
             StateSeeder::class,
             CitySeeder::class,
             LocalitySeeder::class,
-        ]);
+        ]);*/
 
-        Category::factory(3)
-                ->create();
+        State::factory(10)->create();
+
+        City::factory(10)->create();
+
+        Locality::factory(10)->create();
+
+        Category::factory(3)->create();
 
         Experience::factory(10)->create();
 
-        Service::factory(10)->create();
+        Service::factory(10)->hasMicroservices(5)->create();
 
 
     }
