bb07892de05ce42fe7588a2c125162b5b139081a
Johan Tovar <jtovarto@gmail.com>
2021-01-15 11:20:13 -0400

    fix: Mixea css y js en capa guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,8 +12,8 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/embla.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/embla.css') }}">
 
         @stack('styles')
         <style>
@@ -33,7 +33,7 @@
         <x-footer />
 
         @stack('scripts')
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
 
     </body>
 
