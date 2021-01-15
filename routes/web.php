c15388ceb949170a370ab4e1d994860cf663240c
Johan Tovar <jtovarto@gmail.com>
2021-01-15 08:57:30 -0400

    chore: Agrega rutas de prueba

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Livewire\ServiceBuilderWizard;
+use App\Models\City;
+use App\Models\State;
+use App\Models\Service;
+use App\Models\Experience;
 use App\Http\Livewire\WizardCreator;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,26 +22,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::view('/', 'index')->name('index');
-Route::view('/2', 'index2')->name('index2');
+Route::get('/', function () {
+    return view('index', [
+        'services' => App\Models\Service::where('status', STATUS_PUBLISHED)->GET(),
+        'experiences' => App\Models\Experience::where('status', STATUS_PUBLISHED)->GET()
+    ]);
+})->name('index');
 
-Route::view('/experience', 'pages.experience')->name('experience');
-Route::view('/service   ', 'pages.service')->name('service    ');
+
+
 
 Route::get('/listing', function () {
     return view('pages.listing');
-});
+})->name('search');
 
-
+/*
 Route::get('/creator2', function () {
     return view('profile.creator');
 });
+*/
+Route::get('/product/{type}/{id}/{slug}', function ($type, $id) {
 
-Route::get('/product/{id}', function () {
-    return 'Funciona';
+    if ($type == TYPE_SERVICE) {
+        $product = Service::findOrFail($id);
+    } else if ($type == TYPE_EXPERIENCE) {
+        $product = Experience::findOrFail($id);
+    }
+
+    return view("pages.{$type}", compact('product'));
 })->name('product');
+
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+Route::get('/test', function () {
+
+    return view('auth.auth-screen-layout');
+});
+
+
+Route::get('/services/{service}/create', ServiceBuilderWizard::class);
