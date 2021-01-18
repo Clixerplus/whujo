4be72907c279b8a00a1ca28b66d56a72a2104d73
Johan Tovar <jtovarto@gmail.com>
2021-01-17 23:46:01 -0400

    fix(UI): Mejora en adaptacion de tamaño CardV01

--- resources/views/components/card-v01.blade.php
+++ resources/views/components/card-v01.blade.php
@@ -1,4 +1,4 @@
-<div class="relative overflow-hidden duration-300 bg-primary w-72 shadow-w1" style="border-radius: 50px 0 50px 0;">
+<div class="relative overflow-hidden duration-300 bg-primary w-full lg:w-72 shadow-w1" style="border-radius: 50px 0 50px 0;">
     <div class="group">
         <img src="https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500" class="object-cover w-full h-64" alt="" />
         <div
@@ -21,19 +21,19 @@
         </div>
         <div class="flex items-center mb-3">
             <span class="leading-6 flex mr-4">
-                <x-icon-warning class="text-secondary fill-current h-auto w-6 mr-2" />
-                {{ $product->state_id }}
+                <x-icon-map-outline class="text-secondary fill-current h-auto w-6 mr-2" />
+                {{ $product->city->name }}
             </span>
             <span class="leading-6 flex">
-                <x-icon-search class="text-secondary fill-current h-auto w-6 mr-2" />
-                {{ $product->price }}
+                <x-icon-cash-outline class="text-secondary fill-current h-auto w-6 mr-2" />
+                ${{ number_format($product->price,2,',','.') }}
             </span>
         </div>
         <p class="mb-2 text-gray-700 text-sm h-44 ">
             @if(Str::length('jh') < 250)
                  {{ $product->description }}
             @else
-                {{ substr($product->description , 1, 250 ) + '. . .' }};
+                {{ substr($product->description , 1, 200 ) + '. . .' }};
             @endif
 
         </p>
