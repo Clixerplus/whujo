f92a758bb288d834985c37c49129dee3e2367cb2
Johan Tovar <jtovarto@gmail.com>
2021-01-29 11:40:27 -0400

    feat: Add softdelete and new scope to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Models\Experience;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name'
@@ -22,5 +23,10 @@ class Category extends Model
     public function services()
     {
         $this->hasMany(Service::class);
+    }
+
+    public function scopeOnlyActive($query)
+    {
+        return $this->all();
     }
 }
