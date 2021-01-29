0ab1fdabe983b37872a45b87fa1a72bc940eb664
Johan Tovar <jtovarto@gmail.com>
2021-01-29 11:39:26 -0400

    fix: Move the dashboard view to its respective directory

--- resources/views/account/dashboard.blade.php
+++ resources/views/account/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -11,22 +12,24 @@
                 <div class="flex flex-col flex-wrap md:flex-row md:justify-center md:gap-4 w-full lg:p-4 ">
 
                     <x-btn-touch href="#" :icon="'icon-person-add-outline'"
-                        class="w-full md:w-2/5 lg:w-1/5 text-blue-600">
-                        Datos Personales
+                        class="w-full md:w-2/5 lg:w-1/5 text-yellow-500 lg:border-l-4 shadow lg:border-yellow-500">
+                        Perfil
                         <x-slot name="description">
                             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, doloribus
                             neque ipsum expedita?
                         </x-slot>
                     </x-btn-touch>
 
-                    <x-btn-touch href="#" :icon="'icon-key-outline'" class="w-full md:w-2/5 lg:w-1/5 text-primary">
+                    <x-btn-touch href="#" :icon="'icon-key-outline'"
+                        class="w-full md:w-2/5 lg:w-1/5 text-primary lg:border-l-4 shadow lg:border-primary">
                         Seguridad
                         <x-slot name="description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, fuga officiis.
                         </x-slot>
                     </x-btn-touch>
 
-                    <x-btn-touch href="#" :icon="'icon-wallet-outline'" class="w-full md:w-2/5 lg:w-1/5 text-green-500">
+                    <x-btn-touch href="#" :icon="'icon-wallet-outline'"
+                        class="w-full md:w-2/5 lg:w-1/5 text-purple-500 lg:border-l-4 shadow lg:border-purple-500">
                         Pagos y Cobros
                         <x-slot name="description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -34,14 +37,15 @@
                     </x-btn-touch>
 
                     <x-btn-touch href="#" :icon="'icon-notifications-outline'"
-                        class="w-full md:w-2/5 lg:w-1/5 text-yellow-300">
+                        class="w-full md:w-2/5 lg:w-1/5 text-pink-700 lg:border-l-4 shadow lg:border-pink-700">
                         Mensajes y Notificaciones
                         <x-slot name="description">
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam esse quod iure?
                         </x-slot>
                     </x-btn-touch>
 
-                    <x-btn-touch class="w-full md:w-2/5 lg:w-2/5" :icon="'icon-whujo'" href="#">
+                    <x-btn-touch href="{{ route('service-listing') }}" :icon="'icon-newspaper'"
+                        class="w-full md:w-2/5 lg:w-2/5 text-blue-700 lg:border-l-4 shadow lg:border-blue-700">
                         Anuncios de Servicios
                         <x-slot name="description">
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam esse quod
@@ -49,7 +53,8 @@
                         </x-slot>
                     </x-btn-touch>
 
-                    <x-btn-touch class="w-full md:w-2/5 lg:w-2/5" :icon="'icon-whujo'" href="#">
+                    <x-btn-touch href="#" :icon="'icon-rocket'"
+                        class="w-full md:w-2/5 lg:w-2/5 text-green-500 lg:border-l-4 shadow lg:border-green-500">
                         Anuncios de Experiencias
                         <x-slot name="description">
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam esse quod
@@ -57,9 +62,10 @@
                         </x-slot>
                     </x-btn-touch>
 
-
                 </div>
             </div>
         </div>
     </div>
+
+    <x-footer />
 </x-app-layout>
