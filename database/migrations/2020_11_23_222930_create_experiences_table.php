ff8a3638fc990bfaf85bf96318bb223802f9f6e2
Johan Tovar <jtovarto@gmail.com>
2021-01-30 16:46:53 -0400

    fix: Change type column on Experience migrations
    Change string for tinyInteger in groupSize and minimumAge fields

--- database/migrations/2020_11_23_222930_create_experiences_table.php
+++ database/migrations/2020_11_23_222930_create_experiences_table.php
@@ -17,61 +17,61 @@ class CreateExperiencesTable extends Migration
             $table->id();
 
             $table->foreignId('user_id')
-                  ->constrained();
+                ->constrained();
 
             $table->foreignId('category_id')
-                  ->constrained();
+                ->constrained();
 
             $table->string('name')
-                  ->nullable();
+                ->nullable();
 
             $table->text('description')
-                  ->nullable();
+                ->nullable();
 
             $table->jsonb('toProvide')
-                  ->nullable();
+                ->nullable();
 
             $table->jsonb('toBring')
-                  ->nullable();
+                ->nullable();
 
             $table->jsonb('photos')
-                  ->nullable();
+                ->nullable();
 
-            $table->string('minimumAge')
-                  ->nullable();
+            $table->tinyInteger('minimumAge')
+                ->nullable();
 
             $table->enum('skillLevel', config('product.skill_levels'))
-                  ->default(SKILL_LVL_BEGINNER);
+                ->default(SKILL_LVL_BEGINNER);
 
             $table->enum('activityLevel', config('product.activity_levels'))
-                  ->default(ACTIVITY_LVL_LIGHT);
+                ->default(ACTIVITY_LVL_LIGHT);
 
             $table->text('toKnow')
-                  ->nullable();
+                ->nullable();
 
-            $table->string('groupSize')
-                  ->nullable();
+            $table->tinyInteger('groupSize')
+                ->nullable();
 
             $table->jsonb('duration')
-                  ->nullable();
+                ->nullable();
 
             $table->jsonb('starting')
-                  ->nullable();
+                ->nullable();
 
             $table->decimal('price')
-                  ->nullable();
+                ->nullable();
 
             $table->boolean('privateGroup')
-                  ->nullable();
+                ->nullable();
 
             $table->decimal('privateGroupPrice')
-                  ->nullable();
+                ->nullable();
 
             $table->unsignedSmallInteger('reservationLimitTime')
-                  ->nullable();
+                ->nullable();
 
             $table->enum('status', config('product.status'))
-                  ->default(STATUS_INCOMPLETE);
+                ->default(STATUS_INCOMPLETE);
 
             $table->timestamps();
         });
