55bcc96bc3643b24867201625cf448b67f09414c
Johan Tovar <jtovarto@gmail.com>
2021-05-10 07:15:48 -0400

    :build Rebuild css and js files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,9 @@
 const mix = require("laravel-mix");
 
-mix.postCss("resources/css/app.css", "public/css").options({
-    postCss: [require("@tailwindcss/jit")],
-});
+mix.postCss("resources/css/app.css", "public/css")
+    .options({
+        postCss: [require("@tailwindcss/jit")],
+    })
+    .js("resources/js/app.js", "public/js");
 
 mix.browserSync("whujo.test");
