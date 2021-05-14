f070bef2111670ad185b827e502ea32ab38a042a
Johan Tovar <jtovarto@gmail.com>
2021-05-14 13:19:55 -0400

    Update some navbar-top styles

--- resources/views/components/navbar-top.blade.php
+++ resources/views/components/navbar-top.blade.php
@@ -1,24 +1,15 @@
 <div x-data="{open:false}" class="fixed top-0 left-0 w-screen h-20 z-50">
     <div class="relative">
-        <div class="bg-white text-primary p-4 flex items-center">
 
-            <button @click="open = ! open"
-                class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out border rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 lg:hidden">
-                <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round"
-                        stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
-                        stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
+        <div class="bg-white text-primary p-4 flex items-center justify-between shadow">
 
-            <h1 class="h-12 ml-2">
-                <a href="{{ route('home') }}" class="font-whujo text-4xl">
+            <h1 class="h-12">
+                <a href="{{ route('home') }}" class="font-whujo text-4xl font-bold">
                     whujo
                 </a>
             </h1>
 
-            <div class="mx-auto">
+            <div class="ml-auto">
                 <ul class="hidden lg:flex space-x-6">
                     <li class="">Landing page 1</li>
                     <li class="">Landing page 2</li>
@@ -26,32 +17,66 @@
                 </ul>
             </div>
 
-            <div class="h-12 flex items-center ml-auto">
-                <a href="{{ route('login') }}" class="rounded-md border border-red-600 py-1 px-3">
-                    login
-                </a>
+            <button @click="open = ! open"
+                class="text-secondary focus:outline-none lg:hidden">
+                <svg class="w-8 h-8" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            </button>
+
+        </div>
+
+        {{-- dropdown menu --}}
+        <div class="h-screen w-screen overflow-hidden absolute top-0 inset-0 bg-secondary bg-opacity-80 " x-show="open" @click.away="open = false">
+
+            <div class="w-4/5 h-full bg-white p-4"
+                 x-show="open"
+                 x-transition:enter="transition ease-out duration-300" x-transition:enter-start="transform -translate-x-full"
+                 x-transition:enter-end="transform translate-x-" x-transition:leave="ease-in duration-300"
+                 x-transition:leave-start="transform translate-x-1/2" x-transition:leave-end="transform -translate-x-full">
+
+                <div class="flex justify-between">
+                    <a href="{{ route('login') }}" class="px-4 py-2 font-semibold text-white bg-primary rounded-md">Join whujo</a>
+                    <button @click="open = false" class="text-secondary focus:outline-none">
+                        <svg class="w-8 h-8" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path class="text-gray-600" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+
+                <ul class="space-y-3 py-3">
+                    <li>
+                        <a href="#" class="capitalize">{{ __('login') }}</a>
+                    </li>
+                    <li class="capitalize flex justify-between flex-wrap">
+                        <a href="#">{{ __('explore') }}</a>
+                        <x-icon-chevron-forward class="w-4 h-4 transform rotate-90"/>
+                        <ul class="space-y-3 p-3 text-gray-400 w-full">
+                            <li>{{ __('services') }}</li>
+                            <li>{{ __('experiences') }}</li>
+                            <li>{{ __('share a coffe') }}</li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <h3 class="font-semibold text-secondary py-2 mt-8 border-b">{{ __('General') }}</h3>
+                <ul class="space-y-3 text-gray-400 py-3">
+                    <li>
+                        <a href="/" class="capitalize">{{ __('home') }}</a>
+                    </li>
+                    <li>
+                        <a href="#">Landing page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Landing page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Landing page 3</a>
+                    </li>
+                </ul>
+
             </div>
-
-        </div>
-
-        <div class="px-4">
-            <hr>
-        </div>
-
-        <div class="bg-white"
-             x-show="open"
-             x-transition:enter="transition ease-out duration-300"
-             x-transition:enter-start="transform -translate-y-full"
-             x-transition:enter-end="transform translate-y-1/2"
-             x-transition:leave="ease-in duration-300"
-             x-transition:leave-start="transform translate-y-1/2"
-             x-transition:leave-end="transform -translate-y-full">
-
-            <ul class="p-4">
-                <li class="py-3 border-b">Landing page 1</li>
-                <li class="py-3 border-b">Landing page 2</li>
-                <li class="py-3 border-b">Landing page 3</li>
-            </ul>
         </div>
     </div>
 </div>
