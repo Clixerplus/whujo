a9f0f2f24fb91195792bd1c029e87b3aa5e2eb5a
Johan Tovar <jtovarto@gmail.com>
2021-05-23 22:04:13 -0400

    Update button component

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,19 +1,13 @@
 @props([
-
-'classSize' => [
-    'xs' => ' px-2 ',
-    'sm' => ' py-2 px-4 ',
-    'md' => ' py-3 px-5 ',
-    'lg' => ' py-4 px-6 ',
+    'classSize' => [
+        'xs' => ' px-2 ',
+        'sm' => ' py-2 px-4 ',
+        'md' => ' py-3 px-5 ',
+        'lg' => ' py-4 px-6 ',
     ],
-
-'classDefault' => 'flex items-center justify-center placeholder-gray-500 border focus:ring rounded-md
-                   focus:ring-red-200 outline-none text-white focus:outline-none disabled:opacity-25
-                   transition ease-in-out duration-150'
+    'classDefault' => 'outline-none text-white focus:outline-none disabled:opacity-25 transition ease-in-out duration-150 rounded'
 ])
 
-<button {{ $attributes->merge([ 'class' => $classDefault . $classSize[$size] ]) }} >
-
+<button {{ $attributes->merge([ 'class' => $classDefault . $classSize[$size] ]) }}>
     {{ $slot }}
-
 </button>
