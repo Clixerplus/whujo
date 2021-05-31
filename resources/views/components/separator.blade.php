86b1a2ef208d366dbefb7e83caf51e99b7a7bcb3
Johan Tovar <jtovarto@gmail.com>
2021-05-31 18:52:46 -0400

    feat(UI): Add and Update UI Components

---
+++ resources/views/components/separator.blade.php
@@ -0,0 +1,1 @@
+<hr {{ $attributes->merge(['class'=>"hidden my-8 border-4 rounded w-44 border-primary md:block"]) }}>
---
+++ resources/views/components/whujo.blade.php
@@ -0,0 +1,1 @@
+<span {{ $attributes->merge(['class'=>'whujo-brand font-extrabold -mr-1']) }}>whujo</span>
