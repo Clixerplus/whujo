ccac2ece7420b8c8a5f60035885c2d86c15e519c
Johan Tovar <jtovarto@gmail.com>
2021-06-09 01:46:56 -0400

    Update Jetstream

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,23 +1,17 @@
-<x-auth>
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
 
-
-
-
-    <x-slot name="title">
-
-        {{ __('Verify email address') }}
-
-        <p class="mt-4 text-cool-gray-700 text-sm font-normal">
+        <div class="mb-4 text-sm text-gray-600">
             {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-        </p>
-    </x-slot>
-
-    <x-slot name="body">
+        </div>
 
         @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+            </div>
         @endif
 
         <div class="mt-4 flex items-center justify-between">
@@ -25,7 +19,7 @@
                 @csrf
 
                 <div>
-                    <x-jet-button type="submit" class="bg-primary transition ease-in-out duration-500">
+                    <x-jet-button type="submit">
                         {{ __('Resend Verification Email') }}
                     </x-jet-button>
                 </div>
@@ -35,10 +29,9 @@
                 @csrf
 
                 <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Logout') }}
+                    {{ __('Log Out') }}
                 </button>
             </form>
         </div>
-    </x-slot>
-
-</x-auth>
+    </x-jet-authentication-card>
+</x-guest-layout>
